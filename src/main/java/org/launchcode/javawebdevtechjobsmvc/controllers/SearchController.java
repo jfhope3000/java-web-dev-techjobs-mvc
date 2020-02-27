712ab38a7fad0ffa6bd8ff9901b0954469ce1cc1
Jack Hope <jfhope3000@gmail.com>
2020-02-26 23:30:11 -0600

    3 in progress, added searchType.equals(no input)

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -26,7 +26,7 @@ public class SearchController {
     @PostMapping("results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs;
-        if (searchType.toLowerCase().equals("all")) {
+        if (searchType.toLowerCase().equals("all") || searchType.equals("")) {
             jobs = JobData.findAll();
         } else {
             jobs = JobData.findByColumnAndValue(searchType, searchTerm);
